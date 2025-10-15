7efe12a3e3c6aabebe3b0f5e9db2363a92f3346f
admin <wlgus40407046@gmail.com>
2025-10-15 13:21:34 +0900

    5주차 + 6주차 블로그게시판 1,2 완료

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -1,21 +1,59 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import com.example.demo.model.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
-import com.example.demo.model.service.TestService;
+import com.example.demo.model.service.BlogService;
+import java.util.Optional;
+import com.example.demo.model.service.AddArticleRequest;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @Controller
+@Service
+@Repository
+
 public class BlogController {
 
     @Autowired
-    TestService testService;
+    BlogService blogService;
 
     @GetMapping("/article_list")
-    public String article_list() {
+    public String article_list(Model model) {
+        List<Article> list = blogService.findAll();
+        model.addAttribute("articles", list);
         return "article_list";
+    }
+
+    @GetMapping("/article_edit/{id}")
+    public String article_edit(Model model, @PathVariable Long id) {
+        Optional<Article> list = blogService.findById(id);
+
+        if (list.isPresent()) {
+            model.addAttribute("article", list.get());
+        } else {
+            return "/error_page/article_error";
+        }
+        return "article_edit";
+    }
+
+    @PutMapping("/api/article_edit/{id}")
+    public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+        blogService.update(id, request);
+        return "redirect:/article_list";
+    }
+
+    @DeleteMapping("/api/article_delete/{id}")
+    public String deleteArticle(@PathVariable Long id) {
+        blogService.delete(id);
+        return "redirect:/article_list";
     }
 
 }
--- src/main/java/com/example/demo/model/service/BlogService.java
+++ src/main/java/com/example/demo/model/service/BlogService.java
@@ -1,6 +1,7 @@
 package com.example.demo.model.service;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +17,25 @@ public class BlogService {
 
     public List<Article> findAll() {
         return blogRepository.findAll();
+    }
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+    public Optional<Article> findById(Long id) {
+        return blogRepository.findById(id);
+    }
+
+    public void update(Long id, AddArticleRequest request) {
+        Optional<Article> optionalArticle = blogRepository.findById(id);
+        optionalArticle.ifPresent(article -> {
+            article.update(request.getTitle(), request.getContent());
+            blogRepository.save(article);
+        });
+    }
+
+    public void delete(Long id) {
+        blogRepository.deleteById(id);
     }
 }
